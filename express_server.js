03f90e7e8e066ca006ad5923ee8ae200741a5629
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-02-18 00:50:23 +0000

    added random string generator

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,7 +32,23 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 })
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+const generateRandomString = () => {
+
+  return Math.random().toString(36).slice(6)
+}
+
+console.log(generateRandomString())
